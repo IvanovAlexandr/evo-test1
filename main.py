1f73c2b5baa55170a325ebde8488640db4ce6e4e
IvanovAlexandr <ivanov.alexandr.1995@gmail.com>
2016-06-17 17:25:03 +0300

    pep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-
-
 if __name__ == "__main__":
     array = np.random.random_integers(0, 9, (10, 10, 10))
 
@@ -24,7 +22,7 @@ if __name__ == "__main__":
 
             if max_sum_z <= sum_z:
                 max_sum_z = sum_z
-                index_max_sum_z= [j, k]
+                index_max_sum_z = [j, k]
             if max_sum_y <= sum_y:
                 max_sum_y = sum_y
                 index_max_sum_y = [j, k]
@@ -35,7 +33,3 @@ if __name__ == "__main__":
     print("max_x -> [", index_max_sum_x[0], ",", index_max_sum_x[1], ", x ] = ", max_sum_x)
     print("max_y -> [", index_max_sum_y[0], ", y ,", index_max_sum_y[1], "] = ", max_sum_y)
     print("max z -> [ z ,", index_max_sum_z[0], ",", index_max_sum_z[1], "] = ", max_sum_z)
-
-
-
-
